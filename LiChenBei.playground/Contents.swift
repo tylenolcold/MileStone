4092bd0a0ae72596661c979186ed2485ff5e4925
tylenolcold <jimlee1982@hotmail.com>
2016-03-11 00:48:25 +0800

    Added files via upload

---
+++ LiChenBei.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+/*
+2、苹果公司四十年来里程碑式的产品有哪些？
+1976年 Apple 1
+1977年 Apple 2
+1983年 “Lisa”，一款商务用台式计算机
+1984年：苹果推出Macintosh个人计算机
+1998年：苹果公司推出iMac台式电脑
+2001年：苹果公司推出iPod数码音乐随身听
+2003年：iTunes商店开放，允许用户购买和下载音乐、有声书籍、电影和电视节目
+2007年1月：苹果公司推出iPhone手机
+2010年4月：苹果公司开始发售iPad
+2011年3月2日：苹果公司推出iPad 2
+目前官网上的商品有：
+MacBook, MacBook Air, MacBook Pro, iMac, Mac Pro, Mac mini,
+iPad Pro, iPad Air 2, iPad Air, iPad mini 4, iPad mini 2,
+apple watch
+tv
+ipod
+*/
